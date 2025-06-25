5bc0459be2184fafd58b793a2c031cf3371836cb
Thomas Ricouard <ricouard77@gmail.com>
2025-06-25 09:09:03 +0200

    Init project

---
+++ FoundationChat/FoundationChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationChatApp.swift
+//  FoundationChat
+//
+//  Created by Thomas Ricouard on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoundationChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
